50bab42eb6ad50db4b83a7d83b5ae3378f8cb980
dorcas <dorcasaumaakamu@gmail.com>
2024-04-17 18:12:40 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() * 100);
+
+const num3 = 64;
+const num4 = 60;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 10, 20);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
+
